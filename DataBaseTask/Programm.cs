896670c8fe90775b14ba94c6c3a8c005752edd35
Igor Kadach <ikadach91@gmail.com>
2022-09-11 16:51:07 +0300

    add

---
+++ DataBaseTask/Programm.cs
@@ -0,0 +1,93 @@
+﻿using DataBaseTask.Models;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualBasic;
+using NUnit.Framework;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System;
+using System.Collections.Generic;
+using System.Management;
+using System.Collections;
+
+
+namespace DataBaseTask
+{
+    public class Programm
+    {
+        static void Main(string[] args)
+        {
+            using var dbContext = new DBContext();
+            // Get person's email and phone
+            var peoplePhoneEmail = dbContext.People.Include(p => p.PersonPhones).Include(p => p.EmailAddresses).Select(p => p.FirstName).Where(p => p == "Ken").ToList();
+
+            string actualPerson = peoplePhoneEmail[0];
+            string expectedPerson = "Ken";
+            Assert.AreEqual(expectedPerson, actualPerson);
+
+            //Get product name by Id
+            var product = dbContext.Products.Include(p => p.ProductCostHistories).Where(p => p.ProductId == 707).Select(p => p.Name).ToList();
+            
+            string actualProduct = product[0];
+            string expectedProduct = "Sport-100 Helmet, Red";
+            Assert.AreEqual(expectedProduct, actualProduct);
+
+            //Get product id photo 
+            var productPhoto = dbContext.ProductPhotos.Include(p => p.ProductProductPhotos).OrderBy(p => p.ModifiedDate).Select(p => p.ProductPhotoId).Take(10).ToList();
+
+            var actualProductPhoto = productPhoto[5];
+            var expectedProductPhoto = 118;
+            Assert.AreEqual(expectedProductPhoto, actualProductPhoto);
+
+            //Get product name by vendor 
+            var productVendor = dbContext.Products.Include(p => p.ProductVendors).Select(p => p.ProductNumber).Distinct().Take(5).ToList();
+
+            var actualProductVendor = productVendor[1];
+            var expectedProductVendor = "BA-8327";
+            Assert.AreEqual(expectedProductVendor, actualProductVendor);
+
+            //Get currency by currency code
+            var currency = dbContext.Currencies.Include(c => c.CountryRegionCurrencies).Where(c => c.CurrencyCode == "eur" || c.CurrencyCode == "de").Select(c => c.Name).Take(5).ToList();
+
+            var actualCurrency = currency[0];
+            var expectedCurrency = "EURO";
+            Assert.AreEqual(expectedCurrency, actualCurrency);
+
+
+            //Add new person
+            var person = new Person()
+            {
+                PersonType = "IN",
+                FirstName = "FirstName",
+                MiddleName = "MiddleName",
+                LastName = "MiddleName",
+                Suffix = "Suffix",
+                BusinessEntity = new BusinessEntity()
+            };
+
+            dbContext.People.Add(person);
+            dbContext.SaveChanges();
+
+            //Modiffy person 
+            SqlParameter param = new("@BusinessEntityID", 1);
+            SqlParameter param1 = new("@NationalIDNumber", 295847284);
+            SqlParameter param2 = new SqlParameter("@BirthDate", System.Data.SqlDbType.DateTime);
+            param2.Value = DateTime.Now.AddYears(-20);
+            SqlParameter param3 = new("@MaritalStatus", "S");
+            SqlParameter param4 = new("@Gender", "F");
+
+            dbContext.Database.ExecuteSqlRaw("[HumanResources].[uspUpdateEmployeePersonalInfo] @BusinessEntityID,@NationalIDNumber,@BirthDate,@MaritalStatus,@Gender",
+                                  param, param1, param2, param3, param4);
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
